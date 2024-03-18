7898c50222838040ed106ac95d3473ac597df533
shankar661 <shankarbhosale608@gmail.com>
2024-03-18 11:18:20 -0700

    initial commit

---
+++ Seliiiiii/src/test/java/newwww/Newww1.java
@@ -0,0 +1,15 @@
+package newwww;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Newww1 
+{
+	public void mainn()
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+	}
+
+}
