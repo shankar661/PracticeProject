1958d7d991d2c2a723b0192548e4c4a02df69a3c
shankar661 <shankarbhosale608@gmail.com>
2024-03-18 11:59:09 -0700

    cc

--- Seliiiiii/src/test/java/newwww/Newww1.java
+++ Seliiiiii/src/test/java/newwww/Newww1.java
@@ -2,9 +2,11 @@ package newwww;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Newww1 
 {
+	@Test
 	public void mainn()
 	{
 		WebDriver driver=new ChromeDriver();
